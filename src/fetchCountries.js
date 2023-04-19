b78b8827fa3789313d1000879475da1bca84865a
NataliaValchuk <trewax321@gmail.com>
2023-04-19 19:28:55 +0300

    refactored

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,11 +1,13 @@
 const countryList = 'fields=name,capital,population,flags,languages';
 
 export function fetchCountries(name) {
-  return fetch(`https://restcountries.com/v3.1/name/${name}?${countryList}`)
-    .then(response => {
+  return fetch(
+    `https://restcountries.com/v3.1/name/${name}?${countryList}`
+  ).then(response => {
     if (!response.ok) {
       throw new Error(response.status);
     }
+
     return response.json();
   });
 }
--- src/index.js
+++ src/index.js
@@ -8,64 +8,88 @@ const searchBox = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
 
-const cleanMarkup = () => {
+const showInfoNotification = message => Notiflix.Notify.info(message);
+
+const showFailureNotification = message => Notiflix.Notify.failure(message);
+
+const cleanCountryList = () => {
+  countryList.innerHTML = '';
+};
+
+const cleanCountryInfo = () => {
   countryInfo.innerHTML = '';
 };
 
-const searchInput = e => {
-  const searchCountry = e.target.value.trim();
-  if (searchCountry === '') {
-    cleanMarkup();
-    countryList.innerHTML = '';
-    return;
-  }
+const cleanMarkup = () => {
+  cleanCountryList();
+  cleanCountryInfo();
+};
 
-  cleanMarkup();
+const fillCountryList = countries => {
+  countryList.innerHTML = countries
+    .map(
+      e =>
+        `<li><img src="${e.flags.svg}" alt="flag" width="20"> <span>${e.name.official}</span></li>`
+    )
+    .join('');
+};
 
-  fetchCountries(searchCountry)
-    .then(countries => {
-      if (countries.length > 10) {
-        Notiflix.Notify.info(
-          'Too many matches found. Please enter a more specific name.'
-        );
-        return;
-      }
-
-      let result = countries.filter(
-        el => el.name.official.toUpperCase() === searchCountry.toUpperCase()
-      );
-      if (countries.length > 1 && countries.length <= 10 && result) {
-        countryList.innerHTML = countries
-          .map(
-            e =>
-              `<li><img src="${e.flags.svg}" alt="flag" width="20"> <span>${e.name.official}</span></li>`
-          )
-          .join('');
-        return;
-      }
-      if (result) {
-        result = 0;
-      }
-
-      const country = countries[result];
-
-      countryList.innerHTML = '';
-      countryInfo.innerHTML = `<p><img src="${country.flags.svg}"
+const fillCountryInfo = country => {
+  countryInfo.innerHTML = `<p><img src="${country.flags.svg}"
        alt="flag" height="25">
        <span class="title"> ${country.name.official}</span></p>
        <p><strong>Capital:</strong> ${country.capital.join(', ')}</p>
        <p><strong>Population:</strong> ${country.population}</p>
-       <p><strong>Languages:</strong> ${Object.values(country.languages).join(', ')}</p>`;
-    })
-    .catch(error => {
-      if (error.status && error.status === 404) {
-        Notiflix.Notify.failure('Oops, there is no country with that name');
-      } else {
-        Notiflix.Notify.failure('Oops, something went wrong!');
-      }
-      console.error(error);
-    });
+       <p><strong>Languages:</strong> ${Object.values(country.languages).join(
+         ', '
+       )}</p>`;
+};
+
+const handleCountries = countries => {
+  const countriesLength = countries.length;
+
+  if (countriesLength > 10) {
+    showInfoNotification(
+      'Too many matches found. Please enter a more specific name.'
+    );
+
+    return;
+  }
+
+  if (countriesLength > 1) {
+    fillCountryList(countries);
+
+    return;
+  }
+
+  cleanCountryList();
+  fillCountryInfo(countries[0]);
+};
+
+const handleError = error => {
+  cleanMarkup();
+
+  showFailureNotification(
+    error.message === '404'
+      ? 'Oops, there is no country with that name'
+      : 'Oops, something went wrong!'
+  );
+
+  console.error(error);
+};
+
+const searchInput = e => {
+  const value = e.target.value.trim();
+
+  if (value === '') {
+    cleanMarkup();
+
+    return;
+  }
+
+  cleanCountryInfo();
+
+  fetchCountries(value).then(handleCountries).catch(handleError);
 };
 
 searchBox.addEventListener('input', debounce(searchInput, DEBOUNCE_DELAY));
-
